1096ce824d2cf60df071fed27cda10a314a11f32
richard augusto bueno <richard_bueno@ymail.com>
2022-12-08 22:19:23 -0300

    Add project files.

---
+++ BookingService/Core/Domain/Domain/Entities/Booking.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Booking
+    {
+        public int id { get; set; }
+        public DateTime PlacedAt { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        private Status Status { get; set; }
+        public Status CurrentStatus { get { return this.Status; } }
+    }
+}
---
+++ BookingService/Core/Domain/Domain/Enums/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Enums
+{
+    public enum Status
+    {
+        Created = 0,
+        Paid = 1,
+        Finishied = 2,
+        Canceled = 3,
+        Refounded = 4
+    }
+}
